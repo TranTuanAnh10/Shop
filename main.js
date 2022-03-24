bc1248ecda81f26a0d74f0d90ee2f05cbe1e82f2
TranTuanAnh10 <ttanhhh.1010@gmail.com>
2022-03-24 17:14:27 +0700

    hoc ngay 24/3

--- main.js
+++ main.js
@@ -10,3 +10,11 @@ document.getElementById("back-register").onclick = function () {
 document.getElementById("back-login").onclick = function () {
     document.getElementById("login").style.display = 'none';
 };
+document.getElementById("btn-login").onclick = function () {
+    document.getElementById("login").style.display = 'block';
+    document.getElementById("register").style.display = 'none';
+};
+document.getElementById("btn-register").onclick = function () {
+    document.getElementById("register").style.display = 'block';
+    document.getElementById("login").style.display = 'none';
+};
